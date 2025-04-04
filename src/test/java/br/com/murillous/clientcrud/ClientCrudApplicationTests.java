8fde3947c400dc4b8c5a83cf3bc2bf6f108468f6
murillous01 <murillous01@gmail.com>
2025-04-04 13:48:12 -0300

    first commit

---
+++ src/test/java/br/com/murillous/clientcrud/ClientCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.murillous.clientcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
