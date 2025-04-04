28ab6f6ec53f4bd8e0642e5056bc8706da94a564
murillous01 <murillous01@gmail.com>
2025-04-04 13:51:04 -0300

    adicionando entidade cliente

---
+++ src/main/java/br/com/murillous/clientcrud/entities/Client.java
@@ -0,0 +1,28 @@
+package br.com.murillous.clientcrud.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Client implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String cpf;
+
+    private Double income;
+
+    private LocalDate birthDate;
+
+    private Integer children;
+}
